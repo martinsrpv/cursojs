3a9705ab89e2d4116c3d96ae15fe5e1c529b770a
Francis Martins <martinsrpv@gmail.com>
2022-07-04 23:19:54 -0300

    commit

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,21 @@
-alert('Hello')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        // BOM DIA
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#fbb02e'
+    } else if (hora >= 12 && hora < 18){
+        // BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = '#da8e55'
+    } else {
+        // BOA NOITE
+        img.src = 'fotonoite.png'
+        document.body.style.background = '9c7cb3'
+    }
+}

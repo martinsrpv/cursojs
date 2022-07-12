9d9c8e0ac03ffb230ca8b1036bbcb5186b5b5340
Francis Martins <martinsrpv@gmail.com>
2022-07-11 23:29:05 -0300

    Update script.js

--- aula12ex/ex016/script.js
+++ aula12ex/ex016/script.js
@@ -9,12 +9,42 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // CRIANÇA
+                img.setAttribute('src', 'fotobebeh.png')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'fotojovemh.png')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'fotoadulto.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'fotoidoso.png')
+            }
+                
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // CRIANÇA
+                img.setAttribute('src', 'fotobebem.png')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'fotojovemm.png')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'fotoadulta.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'fotoidosa.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }

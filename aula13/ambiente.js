f3884d044fe79e8c8136a5ee149af2f0f7bb7a2c
Francis Martins <martinsrpv@gmail.com>
2022-07-12 22:47:05 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+
+
+/*
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+*/
